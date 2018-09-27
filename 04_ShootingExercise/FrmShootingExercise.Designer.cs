3528b70b8394dc3a7ac5f0ae110395df1489e212
jmubago <jmubagosotomayor@gmail.com>
2018-09-27 19:38:34 +0200

    Agregar archivos de proyecto.

---
+++ 04_ShootingExercise/FrmShootingExercise.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _04_ShootingExercise
+{
+    partial class FrmShootingExercise
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Location = new System.Drawing.Point(139, 38);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(132, 32);
+            this.BtnPlay.TabIndex = 0;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(48, 120);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(328, 20);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FrmShootingExercise
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 184);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnPlay);
+            this.Name = "FrmShootingExercise";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ 04_ShootingExercise/FrmShootingExercise.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_ShootingExercise
+{
+    public partial class FrmShootingExercise : Form
+    {
+        public FrmShootingExercise()
+        {
+            InitializeComponent();
+        }
+
+
+        //A man runs a shooting gallery.He charges 25 cents for 3 shots at some clay plates which
+        //break when hit. If a person breaks 3 plates with his 3 shots he wins a prize which costs the man 1 dollar.
+        //Each broken plate costs the man 4 cents to replace.The probability that a person hits a
+        //plate on a single shot is ½. Write a program to simulate 1000 people playing the game and
+        //determine the average amount of money the man makes/loses per player.
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            double money = -0.12; //initialize with the cost of first 3 plates
+            double platesCost = 0.04;
+
+
+            for (int x = 1; x <= 1000; x++)
+            {
+                money += 0.25; //New game of 3 rounds pays 25 cents
+                int countHits = 0; //Count each time a hit is made
+                for (int i=1; i <= 3; i++)
+                {
+                    int shoot = r.Next(1, 3);
+                    if (shoot == 1)
+                    {
+                        //Hits the target
+                        money -= platesCost;
+                        countHits += 1;
+                    }
+                    if(countHits == 3)
+                    {
+                        //If 3 hits per game, player gets one dollar
+                        money -= 1;
+                    }
+                }
+            }
+            
+            TxtResult.Text="Shooting gallery has: " + Math.Floor(money).ToString() + " dollars after 1000 games";
+        }
+    }
+}
